0f73b63a548f953e2ed510d33f306ab5249e39e8
shanto <santo15-10959@diu.edu.bd>
2022-03-02 15:01:15 +0600

    handle erroe clearly

--- js/index.js
+++ js/index.js
@@ -1,13 +1,12 @@
 const searchPhone = () => {
     const searchField = document.getElementById('search-field');
-    const searchText = searchField.value;
+    const serText = searchField.value;
+    // convert to lower case
+    const searchText = serText.toLowerCase();
     // clear search bar
     searchField.value = '';
-    if(searchText == ''){
-        const error = document.getElementById('warning')
-        error.style.display = "block";
-    }
-    else{
+    // show error massage
+    if(searchText == 'oppo' || searchText == 'samsung' || searchText == 'iphone' ||searchText == 'huawei'){
         const error = document.getElementById('warning')
         error.style.display = "none";
         // load data
@@ -16,11 +15,23 @@ const searchPhone = () => {
         .then(res => res.json())
         .then(data => displaySearchResult(data.data))
         }
+    else{
+        const error = document.getElementById('warning')
+        error.style.display = "block";
+
+        const searchResult = document.getElementById('search-result');
+        searchResult.textContent = '';
+
+        const phoneDetails = document.getElementById('phone-details');
+        phoneDetails.textContent = '';
+
+        
+    }
+   
 }
 
 const displaySearchResult = phones => {
     const phonesSlice = phones.slice(0, 20);
-    console.log(phonesSlice.length);
     const searchResult = document.getElementById('search-result');
     searchResult.textContent = '';
 
@@ -33,7 +44,7 @@ const displaySearchResult = phones => {
             <div class="card-body">
               <h5 class="card-title">${phone.brand}</h5>
               <h6 class="card-title">${phone.phone_name}</h6>
-              <button onclick = "phoneDetails('${phone.slug}')" class= "btn bg-warning">Details</button>
+              <button onclick = "phoneDetails('${phone.slug}')" class= "btn bg-dark text-white fs-5">Details</button>
             </div>
         </div>
         `;
@@ -54,24 +65,33 @@ const displayDetails = (details) => {
     phoneDetails.textContent = '';
 
     const div = document.createElement('div');
+    div.classList.add('d-md-flex', 'justify-content-center');
     div.innerHTML = `
-    <img src="${details.image}" class="card-img-top img-thumbnail" alt="...">
-    <div class="card-body overflow-auto">
+    <div class="w-50">
+        <img class= "w-100" src="${details.image}" class="card-img-top img-thumbnail" alt="...">
+    </div>
+    <div class="card-body overflow-auto w-50">
       <h4 class="card-title">Phone Details</h4>
       <p>${details.releaseDate}</p>
-
-      <h6>Chipset: ${details.mainFeatures.chipSet}</h6>
-      <p>Display: ${details.mainFeatures.displaySize}</p>
-      <h6>Memory: ${details.mainFeatures.memory}</h6>
-      <p>Bluetooth: ${details.others.Bluetooth}</p>
-      <h6>GPS: ${details.others.GPS}</h6>
-      <p></p>
-      <h6></h6>
-      <p>Sensors: ${details.mainFeatures.sensors}</p>
+      <h4>More Details:</h4>
+      <ul>
+        <li>Chipset: ${details.mainFeatures.chipSet}
+        </li>
+        <li>Display: ${details.mainFeatures.displaySize}
+        </li>
+        <li>Memory: ${details.mainFeatures.memory}
+        </li>
+        <li>Bluetooth: ${details.others.Bluetooth}</li>
+        <li>GPS: ${details.others.GPS}</li>
+        <li>Sensors: ${details.mainFeatures.sensors}</li>
+      </ul>
     </div>
     `;
     phoneDetails.appendChild(div);
     window.scrollTo(0,0);
     
 }
+
+
+
 
